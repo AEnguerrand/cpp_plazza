ad1c94d642f2ebf98c17a5dc314153fd22d24531
Antoine Dury <antoine.dury@epitech.eu>
2017-04-20 14:55:50 +0200

    NamedPipe first version OK

--- Encapsulation/INamedPipe.hpp
+++ Encapsulation/INamedPipe.hpp
@@ -5,7 +5,7 @@
 ** Login   <antoine.dury@epitech.eu>
 **
 ** Started on  Tue Apr 18 13:04:19 2017 Antoine Dury
-** Last update Tue Apr 18 14:55:58 2017 Antoine Dury
+** Last update Thu Apr 20 14:47:03 2017 Antoine Dury
 */
 
 #ifndef INAMEDPIPE_HPP
@@ -17,15 +17,23 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <cstring>
+#include <stdlib.h>
 #include "Error.hpp"
+
+struct Test
+{
+  int nb;
+  char str[0];
+};
 
 class INamedPipe
 {
   public:
     virtual ~INamedPipe(void) {}
 
-    virtual void        writeNP(std::string) = 0;
-    virtual std::string readNP(void) = 0;
+    virtual void        create(void) = 0;
+    virtual void        write(void*, size_t) = 0;
+    virtual void        *read(void*, size_t) = 0;
 };
 
 #endif
