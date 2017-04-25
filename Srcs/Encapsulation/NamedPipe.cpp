41f802555bf88cf82183a0440d1837448f59bae2
Antoine Dury <antoine.dury@epitech.eu>
2017-04-25 10:23:08 +0200

    NamedPipe finished

--- Srcs/Encapsulation/NamedPipe.cpp
+++ Srcs/Encapsulation/NamedPipe.cpp
@@ -5,7 +5,7 @@
 ** Login   <antoine.dury@epitech.eu>
 **
 ** Started on  Tue Apr 18 13:54:09 2017 Antoine Dury
-** Last update Thu Apr 20 15:04:18 2017 Antoine Dury
+** Last update Tue Apr 25 10:22:32 2017 Antoine Dury
 */
 
 #include "NamedPipe.hpp"
@@ -18,28 +18,59 @@ NamedPipe::NamedPipe(std::string name)
 
 NamedPipe::~NamedPipe(void) {}
 
-void            NamedPipe::create(void)
+void                NamedPipe::create(void)
 {
   if (mkfifo(this->_fifo.c_str(), 0644) != 0)
     throw Error("Unable to create a named pipe.");
 }
 
-void            NamedPipe::write(void *data, size_t size)
+void                NamedPipe::write(void *data, size_t size)
 {
-  std::fstream  file;
+  std::fstream      fs;
 
-  file.open(this->_fifo, std::ios::out);
-  file.write((char*)data, size);
-  file.close();
+  this->_data = data;
+  this->_size = size;
+  fs << *this;
 }
 
-void            *NamedPipe::read(void *data, size_t size)
+void                *NamedPipe::read(void *data, size_t size)
 {
-  std::fstream  file;
+  std::fstream      fs;
 
-  file.open(this->_fifo, std::ios::in);
-  file.read((char*)data, size);
-  file.close();
-  unlink(this->_fifo.c_str());
+  this->_data = data;
+  this->_size = size;
+  fs >> *this;
   return (data);
+}
+
+const std::string&  NamedPipe::getFifo(void) const
+{
+  return (this->_fifo);
+}
+
+void                *NamedPipe::getData(void) const
+{
+  return (this->_data);
+}
+
+size_t              NamedPipe::getSize(void) const
+{
+  return (this->_size);
+}
+
+std::fstream&       operator<<(std::fstream& fs, const NamedPipe& np)
+{
+  fs.open(np.getFifo(), std::ios::out | std::ios::binary);
+  fs.write((char*)np.getData(), np.getSize());
+  fs.close();
+  return (fs);
+}
+
+std::fstream&       operator>>(std::fstream& fs, const NamedPipe& np)
+{
+  fs.open(np.getFifo(), std::ios::in | std::ios::binary);
+  fs.read((char*)np.getData(), np.getSize());
+  fs.close();
+  unlink(np.getFifo().c_str());
+  return (fs);
 }
