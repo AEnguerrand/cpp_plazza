1d210a5eb3c7b5ec18b52931f40cf34a84da895f
Antoine Dury <antoine.dury@epitech.eu>
2017-04-26 23:05:38 +0200

    Tests NamedPipe

--- Srcs/Encapsulation/NamedPipe.cpp
+++ Srcs/Encapsulation/NamedPipe.cpp
@@ -5,7 +5,7 @@
 ** Login   <antoine.dury@epitech.eu>
 **
 ** Started on  Tue Apr 18 13:54:09 2017 Antoine Dury
-** Last update Wed Apr 26 23:04:18 2017 Antoine Dury
+** Last update Wed Apr 26 23:05:33 2017 Antoine Dury
 */
 
 #include "NamedPipe.hpp"
@@ -38,7 +38,5 @@ void                NamedPipe::writeNP(void *data, size_t size)
 
 void                NamedPipe::readNP(void *data, size_t size)
 {
-  usleep(2500);
   read(this->_fd, reinterpret_cast<char*>(data), size);
-  usleep(2500);
 }
