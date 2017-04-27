db00493f643d99d3e2df595007490a1c1946a2f5
metge_q <quentin.metge@epitech.eu>
2017-04-27 13:52:49 +0200

    gestion erreur sur scrapping

--- Srcs/Scrapper/Scrapper.cpp
+++ Srcs/Scrapper/Scrapper.cpp
@@ -5,7 +5,7 @@
 ** Login   <metge_q@epitech.net>
 **
 ** Started on  Thu Apr 20 14:31:28 2017 Quentin Metge
-** Last update Thu Apr 27 13:45:19 2017 Quentin Metge
+** Last update Thu Apr 27 13:52:30 2017 Quentin Metge
 */
 
 #include "Scrapper.hpp"
@@ -35,16 +35,10 @@ namespace plazza
   /*    Actions    */
   /*****************/
   void                    Scrapper::dispResult(std::string const& str){
-    std::string           res;
+    char                  result[str.size()];
 
-    if (!this->_error.empty())
-      res = "[CE]" + this->_error;
-    else
-      res = "[CO]" + str;
-    char                  result[res.size()];
-
-    str.copy(result, res.size());
-    this->_np->writeNP(result, res.size() * sizeof(char));
+    str.copy(result, str.size());
+    this->_np->writeNP(result, str.size() * sizeof(char));
   }
 
   void                    Scrapper::dispMatch(std::string const& buffer){
@@ -74,7 +68,7 @@ namespace plazza
 
     ss.open(this->_order.fileName);
     if (!ss.is_open()){
-      //std::cerr << "Unable to open file " << this->_order.fileName << "." << std::endl;
+      std::cerr << "Unable to open file " << this->_order.fileName << "." << std::endl;
       return false;
     }
     std::string           line;
